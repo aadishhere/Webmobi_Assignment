850fdf4db9c59a3a15051826932b222cde597599
Aadish Jain <aadishhere@gmail.com>
2024-06-05 14:30:03 +0530

    Initial Commit

---
+++ Webmobi_Assignment/Webmobi_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Webmobi_AssignmentApp.swift
+//  Webmobi_Assignment
+//
+//  Created by Aadish Jain on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Webmobi_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
